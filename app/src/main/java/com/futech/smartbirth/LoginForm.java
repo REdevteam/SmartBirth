59793892fe24e1ccc0033ac931711b97678ea92f
REdevteam <38747679+REdevteam@users.noreply.github.com>
2018-11-20 16:09:52 +0700

    Membuat Layout Login dan Register

--- app/src/main/java/com/futech/smartbirth/LoginForm.java
+++ app/src/main/java/com/futech/smartbirth/LoginForm.java
@@ -1,7 +1,9 @@
 package com.futech.smartbirth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginForm extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class LoginForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+    }
+
+    public void Register(View view) {
+        Intent intent = new Intent(LoginForm.this, RegisterForm.class);
+        startActivity(intent);
     }
 }
